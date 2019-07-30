7dd2de5ded69aa5dcf494ea104212118cedfded5
Michele Freitas <michele-braga@hotmail.com>
2019-07-30 15:46:17 +0100

    Expressao lambda declarada - consumer4

--- src/consumer196/Program.java
+++ src/consumer196/Program.java
@@ -3,9 +3,9 @@ package consumer196;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class Program {
 
@@ -19,9 +19,18 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		//variável recebe valor para atualizar preço.
+		double factor = 1.1;
+		
+		//Consumer que recebe função que recebe produto p e atualiza o preço.
+		//Pode abrir chaves e definir função entre chaves mas com 1 linha é opcional
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+		
 		//vai percorrer a coleção e executar um consumer para cada elemento. Atualizando preço de cada produt
-		//faz referencia a classe não estática criada no Product
-		list.forEach(Product::nonStaticPriceUpdate);
+		//chamando variável cons
+		list.forEach(cons);
 		
 		//imprimir a lista. Usando reference method para o println
 		list.forEach(System.out::println);
